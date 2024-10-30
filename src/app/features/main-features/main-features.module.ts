f74c8cc1d6223c9e629af3f5644440e1a6541db7
Andrea Costa <70828344+Kryostatic94@users.noreply.github.com>
2024-10-30 09:32:25 +0100

    Add files via upload

---
+++ src/app/features/main-features/main-features.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainFeaturesModule { }
